7ad5ad3ee9f2d02857bd95c1fa5ac14f46d4c60c
lizhijiang <lizhijiang@360.cn>
2018-12-20 19:35:35 +0800

    public static 静态属性是属于某个类的

--- src/zealjiang/extend/A.java
+++ src/zealjiang/extend/A.java
@@ -9,4 +9,18 @@ public  class A {
 		B b = new B();
 		b.b();
 	}
+	
+	public static boolean isOpen;
+	
+	@Test
+	public  void  b() {
+		isOpen = true;
+		System.out.println("isOpen: "+isOpen);
+		System.out.println("isOpen: "+A.isOpen);
+		
+		B b = new B();
+		b.getIsOpen();
+		System.out.println("isOpen: "+B.isOpen);
+	}
+	
 }
--- src/zealjiang/extend/B.java
+++ src/zealjiang/extend/B.java
@@ -2,7 +2,13 @@ package zealjiang.extend;
 
 public class B {
 
+	public static boolean isOpen = false;
+	
 	public void b(){
 		System.out.println("b");
 	};
+	
+	public void getIsOpen() {
+		System.out.println("B isOpen:"+isOpen);
+	}
 }
