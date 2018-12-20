f8fe4236ee2d6f49c3166e8ab30fd093fb6e6adb
lizhijiang <lizhijiang@360.cn>
2018-12-20 19:36:29 +0800

    qq

---
+++ src/zealjiang/extend/ParentClass.java
@@ -0,0 +1,15 @@
+package zealjiang.extend;
+
+public class ParentClass {
+
+	private String date = "1960-9-33";
+	
+
+}
+
+class ChildClass extends ParentClass{
+	
+	public static void main(String[] args) {
+		//System.out.println(date);
+	}
+}
